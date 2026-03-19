03ea072d9c1360987ad44800d92ba421a8a63c35
Jeff Rose <jrose0@gmail.com>
2026-03-19 00:16:37 -0400

    Cleanup of npm commands while installing tmux-ide

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({ configFile: resolve(__dirname, 'svelte.config.js') })],
   root: 'src/client',
   build: {
     outDir: '../../public',
